27dada7a36a5f767633ef10e26089d490840c4da
Vilson Borges <vilsonborges98@gmail.com>
2022-05-31 23:44:05 -0300

    Update startup.c
    Criação de prototipos de função como aliais para a função defaltrender para todas as System Exceptions

--- startup.c
+++ startup.c
@@ -5,35 +5,43 @@
 #include <stdint.h>
 
 void reset_handler(void);
-void nmi_handler (void);
+void nmi_handler (void) __attribute__ ((alias("default_handler")));
+void hardfault_handler (void) __attribute__ ((alias("default_handler")));
+void memmanage_handler (void) __attribute__ ((alias("default_handler")));
+void busfault_handler (void) __attribute__ ((alias("default_handler")));
+void usagefault_handler(void) __attribute__ ((alias("default_handler")));
+void svc_handler (void) __attribute__ ((alias("default_handler")));
+void debugmon_handler (void) __attribute__ ((alias("default_handler")));
+void pendsv_handler (void) __attribute__ ((alias("default_handler")));
+void systick_handler (void) __attribute__ ((alias("default_handler")));
 
 uint32_t vectors[] __attribute__((section(".isr_vectors"))) =
 {
     STACK_START, /* 0x0000 0000 */
     (uint32_t)reset_handler, /* 0x0000 0004 */
     (uint32_t)nmi_handler, /* 0x0000 0008 */
-    0, /* 0x0000 000c */
-    0, /* 0x0000 0010 */
-    0, /* 0x0000 0014 */
-    0, /* 0x0000 0018 */
+    (uint32_t)hardfault_handler, /* 0x0000 000c */
+    (uint32_t)memmanage_handler, /* 0x0000 0010 */
+    (uint32_t)busfault_handler, /* 0x0000 0014 */
+    (uint32_t)usagefault_handler, /* 0x0000 0018 */
     0, /* 0x0000 001c */
     0, /* 0x0000 0020 */
     0, /* 0x0000 0024 */
     0, /* 0x0000 0028 */
-    0, /* 0x0000 002c */
-    0, /* 0x0000 0030 */
+    (uint32_t)svc_handler, /* 0x0000 002c */
+    (uint32_t)debugmon_handler, /* 0x0000 0030 */
     0, /* 0x0000 0034 */
-    0, /* 0x0000 0038 */
-    0, /* 0x0000 003c */
+    (uint32_t)pendsv_handler, /* 0x0000 0038 */
+    (uint32_t)systick_handler, /* 0x0000 003c */
 };
+
 
 void reset_handler(void)
 {
-    
+}
+void default_handler(void)
+{
+    while(1){};
 }
 
-void nmi_handler(void)
-{
-while(1){};
-}
 
